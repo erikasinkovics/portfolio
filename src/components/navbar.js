63d1fe0edd478a8ce9bd71c7b7af2fe41b72dc50
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-09-19 21:10:57 -0400

    Navbar styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Navbar = () => (
-     <div>
-          
+     <div className="navbar">
+          <div className="navbar-home">
+               <h2>erika</h2>
+          </div>
+          <div className="navbar-navigation">
+               <Link to="#">About</Link>
+               <Link to="#">Skills</Link>
+               <Link to="#">Portfolio</Link>
+               <Link to="#">Contact Me</Link>
+          </div>
      </div>
 )
    
