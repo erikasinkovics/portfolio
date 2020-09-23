97c4901c35fe940242b2e7f086ba2de0aa522faa
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-09-23 11:29:22 -0400

    Added job title

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,8 +4,9 @@ import { Link } from "gatsby"
 const Intro = () => (
   <div className="intro">
     <h1>Erika Sinkovics</h1>
-    <p>Metus massa tellus sed sed leo, turpis egestas. Mi commodo proin purus mi, sit ut sit at enim. Interdum elementum odio dolor varius dui quam at. Mi commodo proin purus mi, sit ut sit at enim. Interdum elementum odio dolor varius dui quam at.</p>
-    <Link to="#">My resumé</Link>
+    <p id="jobtitle">Full Stack Developer</p>
+    <p>Web developer based in Montréal, Québec</p>
+    <Link to="#">See my resumé</Link>
   </div>
 )
 
