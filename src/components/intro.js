c97708fc600f27db7010c13cb4bf661e4a98a347
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-09-23 22:07:51 -0400

    Changed layout of intro, added lorem, added FA icon

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,12 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons'
+
 
 const Intro = () => (
   <div className="intro">
     <h1>Erika Sinkovics</h1>
     <p id="jobtitle">Full Stack Developer</p>
-    <p>Web developer based in Montréal, Québec</p>
-    <Link to="#">Download my CV</Link>
+    <p>Metus massa tellus sed sed leo, turpis egestas. Mi commodo proin purus mi, sit ut sit at enim. Interdum elementum odio dolor varius dui quam at. Mi commodo proin purus mi, sit ut sit at enim. Interdum elementum odio dolor varius dui quam at.</p>
+    <Link to="#">
+      Download my CV<FontAwesomeIcon icon={faFileDownload} />
+    </Link>
   </div>
 )
 
