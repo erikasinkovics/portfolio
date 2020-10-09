d27f3195ae0abbd96c0c5c70a5a68a246518de5b
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-10-09 16:55:46 -0400

    Rearranged intro section

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,19 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFileDownload } from '@fortawesome/free-solid-svg-icons'
-import ProfilePicture from "../components/profile-picture.js"
 
 
 const Intro = () => (
   <div className="intro">
-    <h1>Erika Sinkovics</h1>
-    <p id="jobtitle">Full Stack Developer</p>
-    <p>Metus massa tellus sed sed leo, turpis egestas. Mi commodo proin purus mi, sit ut sit at enim. Interdum elementum odio dolor varius dui quam at. Mi commodo proin purus mi, sit ut sit at enim. Interdum elementum odio dolor varius dui quam at.</p>
-    <Link to="#">
-      Download my CV<FontAwesomeIcon icon={faFileDownload} />
-    </Link>
-    <ProfilePicture/>
+    <div className ="intro-content">
+      <h4 style={{fontWeight: "400", marginBottom: "1.5rem"}}>hi! 👋 my name is</h4>
+      <h1 style={{letterSpacing: 0.5}}>Erika Sinkovics</h1>
+      <p style={{fontWeight: "300", marginTop: "1rem", fontSize: "1.1rem", letterSpacing: 0.1, lineHeight: "1.4"}}>
+        I'm a full-stack web developer with a passion for back-end, who finds a lot of joy in solving problems and creating value for users.
+      </p>
+      <Link to="#">
+        look at my portfolio
+      </Link>
+    </div>
   </div>
 )
 
