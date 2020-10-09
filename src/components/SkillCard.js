e545dc5ec993a05fed5edcb980d60c08cda63c12
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-10-09 16:59:21 -0400

    Styling skills section

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -2,12 +2,12 @@ import React from "react"
 
 const SkillCard = (props) => (
   <div className="skill-card">
-    <div className="skill-icon">
+    {/* <div className="skill-icon">
       <img src={props.icon} alt={props.icon_text} />
-    </div>
+    </div> */}
     <div className="skill-card-text">
       <h4>{props.title}</h4>
-      <p>{props.description}</p>
+      {props.description}
     </div>
   </div>
 )
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,10 +1,6 @@
 import React from "react"
 import SkillCard from "./SkillCard.js"
 
-import LanguagesImg from "../images/languages.svg"
-import FrontendImg from "../images/frontend.svg"
-import BackendImg from "../images/backend.svg"
-
 const Skills = () => (
   <div className="skills">
     <div className="skills-container">
@@ -12,20 +8,50 @@ const Skills = () => (
       <h2><span>{"<"}</span>Skillset<span>{"/>"}</span></h2>
       <div className="skill-list">
         <SkillCard
-          icon={LanguagesImg}
-          icon_text="Frameworks & Languages"
-          title="Frameworks & Languages" 
-          description="Ruby・Rails・JavaScript ES6・ReactJS・AJAX・OOP・MVC pattern・Regex・git & GitHub・command line・npm・yarn"/>
+          title="Languages"
+          description={
+            <ul>
+              <li>Ruby</li>
+              <li>JavaScript ES6</li>
+              <li>HTML</li>
+              <li>CSS & Sass</li>
+              <li>SQLite</li>
+              <li>PostgreSQL</li>
+            </ul>
+          }/>
         <SkillCard
-          icon={FrontendImg}
-          icon_text="Front-end Tools"
-          title="Front-end Tools"
-          description="HTML・CSS・SCSS / Sass・Bootsrap・Prototyping and wireframing・Figma"/>
+          title="Frameworks"
+          description={
+            <ul>
+              <li>Ruby on Rails</li>
+              <li>ReactJS</li>
+              <li>GatsbyJS</li>
+              <li>Bootstrap</li>
+              <li>AJAX</li>
+              {/* <li>JQuery</li> */}
+            </ul>
+          } />
         <SkillCard
-          icon={BackendImg}
-          icon_text="Back-end Tools"
-          title="Back-end Tools"
-          description="Relational databases・SQLite・PostgreSQL・Heroku・Cloudinary・APIs, JSON scraping"/>
+          title="Tools"
+          description={
+            <ul>
+              <li>git / GitHub</li>
+              <li>command line</li>
+              <li>npm</li>
+              <li>yarn</li>
+              <li>Regex</li>
+            </ul>
+          }/>
+          <SkillCard
+          title="Design"
+          description={
+            <ul>
+              <li>Prototyping</li>
+              <li>Wireframing</li>
+              <li>Figma</li>
+              {/* <li>Whimsical</li> */}
+            </ul>
+          }/>
       </div>
     </div>
   </div>
