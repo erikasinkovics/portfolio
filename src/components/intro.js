a4f8ef2df8764ef94a5c640f74e644fc3413029e
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-10-22 19:06:35 -0400

    changed wording in intro section

--- src/components/intro.js
+++ src/components/intro.js
@@ -10,7 +10,7 @@ const Intro = () => (
           <h4 style={{fontWeight: "400", marginBottom: "1.5rem"}}>hi! 👋 my name is</h4>
           <h1 style={{letterSpacing: 0.5}}>Erika Sinkovics</h1>
           <p style={{fontWeight: "300", marginTop: "1rem", fontSize: "1.1rem", letterSpacing: 0.1, lineHeight: "1.4"}}>
-            I'm a full-stack web developer with a passion for back-end, who finds a lot of joy in solving problems and creating value for users.
+            I'm a full-stack web developer who finds a lot of joy in solving problems and creating value for users.
           </p>
           <Link to="/#projects" className="projects-btn">
             Check out my projects!
